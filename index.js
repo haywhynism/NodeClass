9934c6c603b4dcec7215c985dee0d55baaa23499
haywhynism <85647324+haywhynism@users.noreply.github.com>
2026-09-09 10:17:30 +0100

    tuesday class

--- index.js
+++ index.js
@@ -3,11 +3,44 @@ const express = require("express")
 //create the express application
 const app = express();
 
+const {add, substract} = require("./math")
+
+
 //define the PORT that your application listens on
-const PORT = 3000;
+const PORT = 3001;
+
+console.log(add(5-8));
+
+
+app.get("/", (req,res)=>{
+    res.send("Homepage")
+})
+
+app.get("/web", (req,res) =>{
+    res.send("This is my page")
+})
+
+//route parameters
+app.get("/users/:id",(req, res) => {
+    res.send(`you requested user with ID: ${req.params.id}`)
+})
+
+//Query strings
+app.get("/search", (req, res)=>{
+    res.send(`You searched for: ${req.query}.terms`)
+})
+
+app.get("index", (req, res)=> {
+    res.sendFile(path.join(__dirname, "public", "index.html"))
+})
+
+// app.get("/user", (req, res) =>{
+//     res.send(name: "ade", user: "two")
+// }) 
+
+
 
 //START your server
-
 app.listen(PORT, ()=> {
     console.log(`APP IS RUNNING ON PORT ${PORT}`);
     
---
+++ note.md
@@ -0,0 +1,24 @@
+<!-- Request and response -->
+
+what is inside request?
+
+every HTTP request has a few key parts that matter to us as backend develoers
+
+1.  Method
+2.  url / path
+3.  headers
+
+whats ius inside respomse
+stateus code--> number tellig the client whether the request succeeded or failed e.g 200(ok) or 404 (not found)
+body ---> the actual content being sent back: HTML, JSOn, plain text, a file and so on
+
+JSON- javascript object notation
+
+node.js is a runtime environment for javascript that allows developers to run javascript everywhere for them to build web app, servers and alot
+
+
+GET REQUESTS
+A GET request is used whenever a client wants to retrieve data from the server, 
+
+
+res.semd()
